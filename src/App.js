6912fafed788c1409208f76a58a76312c8d438f4
Sourav Basak <sourav.new2016@gmail.com>
2025-07-08 22:49:06 +0530

    completed xexpensetracker

--- src/App.js
+++ src/App.js
@@ -3,23 +3,46 @@ import WebWrapper from "./layout/WebWrapper/WebWrapper";
 import "./App.css";
 import Transactions from "./components/Transactions/Transactions";
 import TopExpenses from "./components/TopExpenses/TopExpenses";
+import { useEffect, useState } from "react";
+import { TotalContext } from "./context/TotalContext";
 
 function App() {
+  const _balance = useState("5000");
+  const _expense = useState("0");
+  const _expenses = useState([]);
+  useEffect(() => {
+    _expenses[1](JSON.parse(localStorage.getItem("expenses")));
+    _balance[1](JSON.parse(localStorage.getItem("balance")));
+  }, []);
+
+  useEffect(() => {
+    _expenses[0] &&
+      localStorage.setItem("expenses", JSON.stringify(_expenses[0]));
+    _balance[0] && localStorage.setItem("balance", _balance[0]);
+  }, [_balance[0], _expenses[0]]);
   return (
-    <WebWrapper>
-      <h1 className="expense_tracker_heading">Expense Tracker</h1>
-      <ExpenseTracker />
-      <div className="bottom_container">
-        <div className="transactions_wrapper">
-          <h3 className="transactions_heading">Recent Transactions</h3>
-          <Transactions />
+    <TotalContext.Provider
+      value={{
+        _balance,
+        _expense,
+        _expenses,
+      }}
+    >
+      <WebWrapper>
+        <h1 className="expense_tracker_heading">Expense Tracker</h1>
+        <ExpenseTracker />
+        <div className="bottom_container">
+          <div className="transactions_wrapper">
+            <h3 className="transactions_heading">Recent Transactions</h3>
+            <Transactions />
+          </div>
+          <div className="top_expenses_wrapper">
+            <h3 className="top_expenses_heading">Top Expenses</h3>
+            <TopExpenses />
+          </div>
         </div>
-        <div className="top_expenses_wrapper">
-          <h3 className="top_expenses_heading">Top Expenses</h3>
-          <TopExpenses />
-        </div>
-      </div>
-    </WebWrapper>
+      </WebWrapper>
+    </TotalContext.Provider>
   );
 }
 
--- src/layout/WebWrapper/WebWrapper.jsx
+++ src/layout/WebWrapper/WebWrapper.jsx
@@ -1,34 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./WebWrapper.module.css";
-import { TotalContext } from "../../context/TotalContext";
 
 function WebWrapper({ children }) {
-  const _balance = useState("5000");
-  const _expense = useState("0");
-  const _expenses = useState([]);
-  useEffect(() => {
-    _expenses[1](JSON.parse(localStorage.getItem("expenses")));
-    _balance[1](JSON.parse(localStorage.getItem("balance")));
-  }, []);
-
-  useEffect(() => {
-    _expenses[0] &&
-      localStorage.setItem("expenses", JSON.stringify(_expenses[0]));
-    _balance[0] && localStorage.setItem("balance", _balance[0]);
-  }, [_balance[0], _expenses[0]]);
-  return (
-    <div className={styles.webwrapper}>
-      <TotalContext.Provider
-        value={{
-          _balance,
-          _expense,
-          _expenses,
-        }}
-      >
-        {children}
-      </TotalContext.Provider>
-    </div>
-  );
+  return <div className={styles.webwrapper}>{children}</div>;
 }
 
 export default WebWrapper;
